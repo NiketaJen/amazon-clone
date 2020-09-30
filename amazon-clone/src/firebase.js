8022fbf577cafa385364a7ab23821684aa1d7c87
NiketaJen <niketa.jenerette@gmail.com>
2020-09-30 14:54:59 -0500

    header, home, and checkout pages

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAHU494FTQlNjAIZcawujzmSuXuQSWS3FY",
+    authDomain: "clone-4c78a.firebaseapp.com",
+    databaseURL: "https://clone-4c78a.firebaseio.com",
+    projectId: "clone-4c78a",
+    storageBucket: "clone-4c78a.appspot.com",
+    messagingSenderId: "966228222431",
+    appId: "1:966228222431:web:eed983ecd30805fe80e46a",
+    measurementId: "G-JG2NPRX0YC"
+  };
